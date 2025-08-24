16638b2a49192bf77314499baa45dfc14bb75426
Dhruv278 <dhruvgopani8@gmail.com>
2025-08-24 18:12:44 +0530

    update image path

--- src/utils/images.ts
+++ src/utils/images.ts
@@ -1,6 +1,6 @@
 export const images = {
   HERO_SECTION_IMAGE: "/assets/DummyImage.webp",
-  LOGO_SECTION: "/assets/LOGO_TEXT.png",
+  LOGO_SECTION: "/assets/Logo_Text.png",
   ONLY_LOGO: "/assets/icon.png",
   TEXT_ONLY: "/assets/Text.png",
 };
